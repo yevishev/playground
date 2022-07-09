66b2cad962709b44e1a11fc1f131e88ffb8b0352
yevishev <yevishev.m@gmail.com>
2022-07-09 12:19:32 +0300

    Initial Commit

---
+++ Nba.com clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nba.com clone
+//
+//  Created by oberoya on 09/07/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Nba.com cloneUITests/Nba_com_cloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Nba_com_cloneUITestsLaunchTests.swift
+//  Nba.com cloneUITests
+//
+//  Created by oberoya on 09/07/2022.
+//
+
+import XCTest
+
+class Nba_com_cloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
