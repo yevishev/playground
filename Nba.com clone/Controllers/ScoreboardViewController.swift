7978438744c22f65b6114329948ae54ce7527b26
yevishev <yevishev.m@gmail.com>
2022-07-10 10:43:36 +0300

    added two tables at two viewController

--- Nba.com clone/Controllers/ScoreboardViewController.swift
+++ Nba.com clone/Controllers/ScoreboardViewController.swift
@@ -8,22 +8,46 @@
 import UIKit
 
 class ScoreboardViewController: UIViewController {
-
+    
+    //записываю в константу замыкание для последующего вызова внутри данного класса, которая должна вернуть объект класса UITableView
+    private let scoreboardTableView: UITableView = {
+        let table = UITableView()
+        //в методе register указывается класс, который будет отвечать за логику создания ячейки таблицы
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        view.backgroundColor = .systemRed
+        
+        view.backgroundColor = .systemBackground
+        //как я понял, данный метод добавляет view поверх представления, которое до этого было в самом верху (т.е. superview). В данном случае поверх view с цветом systemBackground накладывается созданная таблица
+        view.addSubview(scoreboardTableView)
+        
+        //idk
+        scoreboardTableView.delegate = self
+        //протокол, который выступает в качестве источника данных для заполнения таблицы. данные для таблицы не сохраняются
+        scoreboardTableView.dataSource = self
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //как я понял, данный метод вызывается в том случае, чтобы необходимо было ViewControllerClass сообщить о том, что появляются ещё дочерний view, которые необходимо отрисовать
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        // свойство frame в каком месте будет располагаться вьюшка, от которого была вызвана. В данном случае, newsTableView возвращает объект класса таблицы, и в свойство frame указывается, что она будет располагаться в границах собственной системы координат (как я понял в границах всего экрана)
+        scoreboardTableView.frame = view.bounds
     }
-    */
-
 }
+
+extension ScoreboardViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "#2 Phoenix suns 2 2"
+        return cell
+    }
+}
+
--- Nba.com clone/MainTabBarViewController.swift
+++ Nba.com clone/MainTabBarViewController.swift
@@ -10,10 +10,28 @@ class MainTabBarViewController: UITabBarController {
         //переопределить цвет фона
         view.backgroundColor = .systemYellow
         
+        //в константе vc1 хранится объект класса viewController, который будет отвечать за представление в том случае, если он будет выбран на tabBar панели
         let vc1 = UINavigationController(rootViewController: NewsViewController())
         let vc2 = UINavigationController(rootViewController: ScoreboardViewController())
         let vc3 = UINavigationController(rootViewController: RankingsViewController())
         let vc4 = UINavigationController(rootViewController: ScheduleViewController())
+        
+        //установить изображения для кнопок взаимодействия
+        vc1.tabBarItem.image = UIImage(systemName: "newspaper")
+        vc2.tabBarItem.image = UIImage(systemName: "list.number")
+        vc3.tabBarItem.image = UIImage(systemName: "chart.bar")
+        vc4.tabBarItem.image = UIImage(systemName: "calendar")
+        
+        //уставновить текст под кнопками
+        vc1.title = "News"
+        vc2.title = "Scoreboard"
+        vc3.title = "Rankings"
+        vc4.title = "Schedule"
+        
+        //цвет тона. параметр tintColor применяет цвет для объектов tabBar панели
+        tabBar.tintColor = .label
+        //Первым параметром метода я устанавливаю массив объектов классов ViewController для tabBarController
+        setViewControllers([vc1, vc2, vc3, vc4], animated: true)
     }
 
 
