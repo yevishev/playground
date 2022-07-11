0c762206fb5aced2111e8eda98dd353ad0d9b403
yevishev <yevishev.m@gmail.com>
2022-07-11 23:32:50 +0300

    end first part of bullseye game

--- Nba.com clone/Controllers/RankingsViewController.swift
+++ Nba.com clone/Controllers/RankingsViewController.swift
@@ -8,17 +8,11 @@
 import UIKit
 
 class RankingsViewController: UIViewController {
-
+    var sliderVal: Float = 0.0
     override func viewDidLoad() {
         super.viewDidLoad()
 
         view.backgroundColor = .systemBackground
-        
-        //button settings
-        let button = UIButton(frame: CGRect(x: 160, y: 500, width: 100, height: 50))
-        button.backgroundColor = .purple
-        button.setTitle("check", for: .normal)
-        button.addTarget(self, action: #selector(showAlert), for: UIControl.Event.touchUpInside)
         
         //slider settings
         let slider = UISlider(frame: CGRect(x: 20, y: 400, width: 380, height: 20))
@@ -27,26 +21,34 @@ class RankingsViewController: UIViewController {
         slider.minimumValue = 0.0;
         slider.maximumValue = 50.0;
         slider.addTarget(self, action: #selector(sliderValue), for: UIControl.Event.valueChanged)
+        
+        //button settings
+        let button = UIButton(frame: CGRect(x: 160, y: 500, width: 100, height: 50))
+        button.backgroundColor = .systemBlue
+        button.setTitle("check", for: .normal)
+        button.addTarget(self, action: #selector(showAlert), for: UIControl.Event.touchUpInside)
         view.addSubview(button)
         view.addSubview(slider)
     }
     
     @objc func sliderValue(slider: UISlider) -> Float {
-        print(slider.value)
-        return slider.value
+        sliderVal = slider.value
+        return sliderVal
     }
     
-    @objc func showAlert() {
-      let alert = UIAlertController(
-        title: "Hello, World",
-        message: "This is my first app!",
-        preferredStyle: .alert)
-      let action = UIAlertAction(
-        title: "Awesome",
-        style: .default,
-        handler: nil)
-      alert.addAction(action)
-      present(alert, animated: true, completion: nil)
+    @objc func showAlert(_ sender: UIButton) {
+        let currentValue: String = String(sliderVal)
+        print(currentValue)
+        let alert = UIAlertController(
+            title: "Hello, World",
+            message: currentValue + " points",
+            preferredStyle: .alert)
+        let action = UIAlertAction(
+            title: "Awesome",
+            style: .default,
+            handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
     }
 
 }
