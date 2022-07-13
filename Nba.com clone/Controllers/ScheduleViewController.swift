c23ae8669ada5c1eaab45651be4bd9917ca18e26
yevishev <yevishev.m@gmail.com>
2022-07-13 22:50:18 +0300

    start of new task. GIF searching

--- Nba.com clone/Controllers/ScheduleViewController.swift
+++ Nba.com clone/Controllers/ScheduleViewController.swift
@@ -7,12 +7,28 @@
 
 import UIKit
 
-class ScheduleViewController: UIViewController {
-
+class ScheduleViewController: UIViewController, UITextFieldDelegate {
+    
+    private let searchTextField = UITextField(frame: CGRect(x: 10, y: 100, width: 394, height: 50))
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        searchTextField.backgroundColor = .white
+        searchTextField.borderStyle = .roundedRect
+        searchTextField.clearButtonMode = .always
+        searchTextField.returnKeyType = .search
+        searchTextField.keyboardType = .default
+        searchTextField.placeholder = "Maximum characters 20"
+        searchTextField.delegate = self
         view.backgroundColor = .systemBackground
+        view.addSubview(searchTextField)
     }
-
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        if let text = textField.text, let textRange = Range(range, in: string) {
+            let updatedText = text.replacingCharacters(in: textRange, with: string)
+            print(updatedText)
+        }
+        return true
+    }
 }
